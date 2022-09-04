08570057fe04c2ec36698b7b8edddaa534653663
Robbinned <ethanr@mentoringacademy.org>
2022-09-03 22:24:55 -0700

    Update split.py

--- split.py
+++ split.py
@@ -1,3 +1,34 @@
+import os
+import shutil
+
+paths = {
+    'label_val' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_two/Labels/val/',
+    'label_train' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_two/Labels/train/',
+    'img_val' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_two/Images/val/',
+    'img_train' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_two/Images/train/',
+    'img' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_one/Images/', #all annotated images
+    'label' : 'C:/Users/highf/Documents/GitHub/squirrel_dataset/dataset_one/Labels/' #all labels
+    }
+
+
+def main():
+    shutil.rmtree(paths['label_val'])
+    shutil.rmtree(paths['label_train'])
+    shutil.rmtree(paths['img_val'])
+    shutil.rmtree(paths['img_train'])
+    
+    os.mkdir(paths['label_val'])
+    os.mkdir(paths['label_train'])
+    os.mkdir(paths['img_val'])
+    os.mkdir(paths['img_train'])
+    
+    
+    num_img = len(os.listdir(paths['img'])) #number of annotated images
+    num_val = int((num_img * 0.1) // 1) #get number of val images to be made
+    
+    img_names = os.listdir(paths['img'])
+    
+    val_img_names = img_names[:num_val]
     train_img_names = img_names[num_val:]
     
     for name in train_img_names:
